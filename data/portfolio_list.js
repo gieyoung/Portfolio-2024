8382449a3155a0b3b63a3ca0adc7d27d423e6f13
gieyoung <159221313+gieyoung@users.noreply.github.com>
2024-08-19 19:21:37 +0900

    c

---
+++ data/portfolio_list.js
@@ -0,0 +1,39 @@
+// 포트폴리오 데이터 JS 
+
+// 1. 미리보기 구성 데이터
+/************************************* 
+    [ 데이터항목 : ]
+    1.순번(기본키) : idx
+    2.카테고리: category
+    3.이미지: img
+    4.제목 : title
+    5.제목하단: sub
+    6.작업기간:date
+    7.관련서류:doc
+    8.설명 : desc
+    9.사용기술: skill
+    10.링크: link
+    
+*************************************/
+
+
+const portfolioData = [
+    {
+      idx: "1",
+      category: "그린",
+      img: "",
+      title: "오뚜기 반응형 홈페이지",
+      sub: "퍼블리싱",
+      date:"",
+      doc:"",
+      desc: "",
+      skill: "HTML5, CSS3, JavaScript",
+      link: "",
+    },
+
+];
+
+
+
+// 데이터 공개하기 //////
+export default portfolioData;
--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 // 메인화면 JS - main.js
 
+// 포트폴리오 리스트 데이터 불러오기 /////
+import portfolioData from "../data/portfolio_list.js";
+// console.log(portfolioData);
+
+
+
+
+
+
+
 
 ///////////[ main2 동영상 열고 닫기 ]/////////////
 
-    $(".v-open").on("click", () => {
-      $(".vid-popup").addClass("on");
-      $(".close-btn").show();
-      $(".vid-popup>table").show();
+    // $(".v-open").on("click", () => {
+    //   $(".vid-popup").addClass("on");
+    //   $(".close-btn").show();
+    //   $(".vid-popup>table").show();
 
-    });
-    $(".close-btn").on("click", () => {
-      $(".vid-popup").removeClass("on");
-      $(".close-btn").hide();
+    // });
+    // $(".close-btn").on("click", () => {
+    //   $(".vid-popup").removeClass("on");
+    //   $(".close-btn").hide();
      
-    });
+    // });
